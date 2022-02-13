259f5fe93e4d4ccedf0a571464a5b8337eff2ca2
M Zulkifli <kiflyfcb7@gmail.com>
2022-02-14 01:55:12 +0800

    new update04

--- 06-Dom/02-coretan.js
+++ 06-Dom/02-coretan.js
@@ -1,3 +1,15 @@
-const btnComment = document.getElementById('btn-comment');
+// const btnComment = document.getElementById('btn-comment');
+const elBtnComment = document.querySelector('#btn-comment');
+const elCommentList = document.querySelector('#comment-list');
 
-btnComment.addEventListener("click");
+const elCommentSection = document.querySelector('#comment-form-section');
+ const elCommentForm = document.querySelector('#comment-form');
+const elInputUsername = document.querySelector('#input-username');
+const elInputComment = document.querySelector('#input-comment');
+const elBtnSubmit = document.querySelector('#btn-submit');
+const elBtnCancel = document.querySelector('#btn-cancel');
+
+elBtnComment.addEventListener("click", showComm);
+function showComm (){
+    elCommentSection.classList.remove("d-none");
+}
