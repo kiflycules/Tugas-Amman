5e3a68cf37040f7291cc036ba3794b61d1b6cc0d
M Zulkifli <kiflyfcb7@gmail.com>
2022-02-14 10:07:51 +0800

    new update05

--- 06-Dom/02-coretan.js
+++ 06-Dom/02-coretan.js
@@ -9,7 +9,33 @@ const elInputComment = document.querySelector('#input-comment');
 const elBtnSubmit = document.querySelector('#btn-submit');
 const elBtnCancel = document.querySelector('#btn-cancel');
 
+// data baru untuk mengambil nama user dan comment user
+let userName = "";
+let userComment ="";
+// let userNameInput = document.getElementById("input-username");
+// let userCommentInput = document.getElementById("input-comment");
+
+// soal-01
 elBtnComment.addEventListener("click", showComm);
 function showComm (){
     elCommentSection.classList.remove("d-none");
+
 }
+
+// soal-02-dan-03
+elCommentForm.addEventListener("submit", function(event){
+    event.preventDefault();
+
+    // const userName = userNameInput.value;
+    // const userComment = userCommentInput.value;
+    // let formatMessage = `(${userName}) ${userComment}`;
+
+
+    // console.log(event);
+    // console.log(userName, userComment);
+    console.log(formatMessage);
+});
+
+// soal-04
+
+
